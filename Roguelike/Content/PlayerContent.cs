b9fbec314cfc183ee14f5f8d71f1838acfab7eed
Alex Chan <chan.730@osu.edu>
2020-05-02 15:25:13 -0400

    Add project files.

---
+++ Roguelike/Content/PlayerContent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    public class PlayerContent
+    {
+
+        ContentManager content;
+
+
+        //player
+        public Texture2D PlayerDefaultSpriteSheet { get; set; }
+        public PlayerContent(ContentManager content)
+        {
+            //this.content = content;
+            PlayerDefaultSpriteSheet = content.Load<Texture2D>("SmileyWalk");
+        }
+
+
+        public void Unload()
+        {
+            content.Unload();
+        }
+
+        
+
+
+    }
+}
---
+++ Roguelike/Interfaces/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike.Interfaces
+{
+    public interface IPlayer
+    {
+        //ISprite PlayerSprite { get; set; }
+
+    }
+}
