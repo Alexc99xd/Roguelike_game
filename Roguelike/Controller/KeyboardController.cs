7d587755db1c180ae90f5f9d25b4cab5eb899854
Alex Chan <chan.730@osu.edu>
2020-05-04 20:41:20 -0400

    keyboard controller possible functionalities

--- Roguelike/Controller/KeyboardController.cs
+++ Roguelike/Controller/KeyboardController.cs
@@ -17,31 +17,40 @@ namespace Roguelike.Controller
 
         public KeyboardController()
         {
-            keyStack = new KeyPressStack<Keys>();
+            //keyStack = new KeyPressStack<Keys>();
         }
 
         public void Update()
         {
+            //we could either do
+            //update every key on every frame (aka) key.execute()
 
+            //or we could do it based on button state
+            //if pressedDown, key.execute() <- has different functionality
+            //if pressedUp, key.DeExecute() <- ends the command state
+
+            //or a bit of both
+            //WASD -> could be the pressedDown/pressedUp
+            //rest of keys could be everyframe
         }
 
-        private void ArrayChangeToStack(Keys[] pressedKeys, Dictionary<Keys, ICommand> mapping)
-        {
-            IEnumerable<Keys> keyUnpressed = prevKeyArray.Except(pressedKeys);
-            IEnumerable<Keys> newKeyPressed = pressedKeys.Except(prevKeyArray);
-            foreach (Keys pressed in newKeyPressed)
-            {
-                if (mapping.ContainsKey(pressed))
-                {
-                    keyStack.Push(pressed);
-                }
+        //private void ArrayChangeToStack(Keys[] pressedKeys, Dictionary<Keys, ICommand> mapping)
+        //{
+        //    IEnumerable<Keys> keyUnpressed = prevKeyArray.Except(pressedKeys);
+        //    IEnumerable<Keys> newKeyPressed = pressedKeys.Except(prevKeyArray);
+        //    foreach (Keys pressed in newKeyPressed)
+        //    {
+        //        if (mapping.ContainsKey(pressed))
+        //        {
+        //            keyStack.Push(pressed);
+        //        }
 
-            }
-            foreach (Keys unpressed in keyUnpressed)
-            {
-                keyStack.Remove(unpressed);
-                //Can up "OnPressUp" (released pressed) command
-            }
-        }
+        //    }
+        //    foreach (Keys unpressed in keyUnpressed)
+        //    {
+        //        keyStack.Remove(unpressed);
+        //        //Can up "OnPressUp" (released pressed) command
+        //    }
+        //}
     }
 }
