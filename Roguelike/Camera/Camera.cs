06c91c982a4bd07b77fb2a16a808532634d6675d
Alex Chan <chan.730@osu.edu>
2020-05-24 21:10:16 -0400

    camera update

--- Roguelike/Camera/Camera.cs
+++ Roguelike/Camera/Camera.cs
@@ -15,38 +15,18 @@ namespace Roguelike.Camera
         public Vector2 Position { get; private set; }
         // Current Zoom level with 1.0f being standard
         public float Zoom { get; private set; }
-        // Current Rotation amount with 0.0f being standard orientation
-        public float Rotation { get; private set; }
 
         // Height and width of the viewport window which we need to adjust
         // any time the player resizes the game window.
         public int ViewportWidth { get; set; }
         public int ViewportHeight { get; set; }
 
-        // Center of the Viewport which does not account for scale
-        public Vector2 ViewportCenter
-        {
-            get
-            {
-                return new Vector2(ViewportWidth * 0.5f, ViewportHeight * 0.5f);
-            }
-        }
+        public Matrix TranslationMatrix {  get => Matrix.CreateTranslation(-(int)Position.X, -(int)Position.Y, 0) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) * Matrix.CreateTranslation(new Vector3(ViewportCenter, 0)); }
 
-        // Create a matrix for the camera to offset everything we draw,
-        // the map and our objects. since the camera coordinates are where
-        // the camera is, we offset everything by the negative of that to simulate
-        // a camera moving. We also cast to integers to avoid filtering artifacts.
-        public Matrix TranslationMatrix
-        {
-            get
-            {
-                return Matrix.CreateTranslation(-(int)Position.X,
-                   -(int)Position.Y, 0) *
-                   Matrix.CreateRotationZ(Rotation) *
-                   Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
-                   Matrix.CreateTranslation(new Vector3(ViewportCenter, 0));
-            }
-        }
+        // Center of the Viewport which does not account for scale
+        public Vector2 ViewportCenter { get => new Vector2(ViewportWidth * 0.5f, ViewportHeight * 0.5f); }
+
+
 
         // Call this method with negative values to zoom out
         // or positive values to zoom in. It looks at the current zoom
